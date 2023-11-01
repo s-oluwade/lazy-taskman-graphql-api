ef8917856598013e9c066b2eab3a7ea355b65f1d
Samuel Oluwade <samueloluwade.dev@gmail.com>
2023-11-01 03:02:23 -0500

    added tasks and subtasks
    added to model, schema and resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -3,11 +3,18 @@ import {Sequelize} from 'sequelize';
 import _Album from './models/Album.ts';
 import _Artist from './models/Artist.ts';
 import _Track from './models/Track.ts';
+import _Task from './models/Task.ts';
+import _Subtask from './models/Subtask.ts';
+import mysql2 from 'mysql2';
 
 const sequelize = new Sequelize(process.env.MYSQLDATABASE!, process.env.MYSQLUSER!, process.env.MYSQLPASSWORD, {
   host: process.env.MYSQLHOST,
   dialect: 'mysql',
-  port: Number(process.env.MYSQLPORT),
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  },
   define: {
     freezeTableName: true,
     timestamps: false,
@@ -19,6 +26,17 @@ const sequelize = new Sequelize(process.env.MYSQLDATABASE!, process.env.MYSQLUSE
     idle: 10000,
   },
 });
+
+sequelize
+  .getQueryInterface()
+  .showAllSchemas()
+  .then((tableObj) => {
+    console.log('// Tables in database', '==========================');
+    console.log(tableObj);
+  })
+  .catch((err) => {
+    console.log('showAllSchemas ERROR', err);
+  });
 
 try {
   await sequelize.authenticate();
@@ -32,6 +50,8 @@ try {
 const Artist = _Artist(sequelize);
 const Album = _Album(sequelize);
 const Track = _Track(sequelize);
+const Task = _Task(sequelize);
+const Subtask = _Subtask(sequelize);
 
 export const resolvers = {
   Query: {
@@ -45,6 +65,10 @@ export const resolvers = {
     album: async (obj: any, args: any, context: any, info: any) => await Album.findByPk(args.id),
     tracks: async () => await Track.findAll(),
     track: async (obj: any, args: any, context: any, info: any) => await Track.findByPk(args.id),
+    tasks: async () => await Task.findAll(),
+    task: async (obj: any, args: any, context: any, info: any) => await Task.findByPk(args.id),
+    subtasks: async () => await Subtask.findAll(),
+    subtask: async (obj: any, args: any, context: any, info: any) => await Subtask.findByPk(args.id),
   },
   Artist: {
     albums: async (parent: any) => (await Album.findAll()).filter((album) => album.artist_id === parent.id),
@@ -56,5 +80,98 @@ export const resolvers = {
   Track: {
     artist: async (parent: any) => await Artist.findOne({where: {id: parent.artist_id}}),
     album: async (parent: any) => await Album.findOne({where: {id: parent.album_id}}),
+  },
+  Task: {
+    subtasks: async (parent: any) => (await Subtask.findAll()).filter((subtask) => subtask.taskId === parent.id),
+  },
+  Subtask: {
+    task: async (parent: any) => await Task.findOne({where: {id: parent.taskId}}),
+  },
+  Mutation: {
+    async deleteTask(_: any, args: any) {
+      const task = await Task.findOne({where: {id: args.id}});
+      task?.destroy();
+      // also delete subtasks
+      await Subtask.destroy({where: {taskId: args.id}});
+      return await Task.findAll();
+    },
+    /*
+     * args.task includes:
+     * title, label, priority, dueDate?
+     */
+    async addTask(_: any, args: any) {
+      const createdTask = await Task.create({
+        ...args.task,
+      });
+
+      await Subtask.create({
+        index: (await Subtask.findAll()).length,
+        taskId: createdTask.id,
+      });
+
+      return createdTask;
+    },
+    async updateTask(_: any, args: any) {
+      await Task.update(args.edits, {where: {id: args.id}});
+
+      return await Task.findOne({where: {id: args.id}});
+    },
+
+    async deleteSubtask(_: any, args: any) {
+      const subtask = await Subtask.findOne({where: {id: args.id}});
+      subtask?.destroy();
+      return await Subtask.findAll();
+    },
+    /*
+     * args.subtask includes:
+     * taskId, title
+     */
+    async addSubtask(_: any, args: any) {
+      return await Subtask.create({
+        index: (await Subtask.findAll()).length,
+        ...args.subtask,
+      });
+    },
+    async updateSubtask(_: any, args: any) {
+      await Subtask.update(args.edits, {where: {id: args.id}});
+
+      return await Subtask.findOne({where: {id: args.id}});
+    },
+    async reorderSubtasks(_: any, args: any) {
+      const taskId = args.taskId;
+      const subtaskId = args.subtaskId;
+      const newIndex = args.newIndex;
+      const subtaskToBeMoved = await Subtask.findOne({where: {id: subtaskId}});
+      if (!subtaskToBeMoved || newIndex === subtaskToBeMoved.index) {
+        return;
+      }
+      const oldIndex = subtaskToBeMoved.index;
+      const subtasks = (await Subtask.findAll()).filter((subtask) => subtask.taskId === taskId);
+
+      // newIndex < subtaskToBeMoved.index, meaning we are moving the subtask backwards
+      if (newIndex < oldIndex) {
+        subtasks.map((subtask) => {
+          if (subtask.id === subtaskToBeMoved.id) {
+            subtask.index = newIndex;
+            return subtask;
+          } else if (subtask.index >= newIndex && subtask.index < oldIndex) {
+            subtask.index += 1;
+            return subtask;
+          }
+        });
+      }
+      // newIndex > subtaskToBeMoved.index, meaning we are moving the subtask forward
+      else {
+        subtasks.map((subtask) => {
+          if (subtask.id === subtaskToBeMoved.id) {
+            subtask.index = newIndex;
+            return subtask;
+          } else if (subtask.index <= newIndex && subtask.index > oldIndex) {
+            subtask.index -= 1;
+            return subtask;
+          }
+        });
+      }
+    },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -10,7 +10,6 @@ export const typeDefs = `#graphql
     external_url: String
     albums: [Album!]
   }
-
   type Album {
     id: Int
     name: String!
@@ -22,7 +21,6 @@ export const typeDefs = `#graphql
     tracks: [Track!]
     artist: Artist!
   }
-
   type Track {
     id: Int
     name: String!
@@ -36,7 +34,25 @@ export const typeDefs = `#graphql
     artist: Artist!
     album: Album!
   }
-
+  type Task {
+    id: Int!
+    title: String!
+    label: String!
+    priority: String!
+    dueDate: String
+    status: String!
+    progress: Int!
+    cursor: Int!
+    subtasks: [Subtask!]!
+  }
+  type Subtask {
+    id: Int!
+    index: Int!
+    taskId: Int!
+    title: String!
+    status: String!
+    task: Task!
+  }
   type Query {
     # employees: [Employee]
     # departments: [Department]
@@ -48,5 +64,41 @@ export const typeDefs = `#graphql
     album(id: Int!): Album
     tracks: [Track]
     track(id: Int!): Track
+    tasks: [Task]
+    task(id: Int!): Task
+    subtasks: [Subtask]
+    subtask(id: Int!): Subtask
+  }
+  type Mutation {
+    addTask(task: AddTaskInput!): Task
+    deleteTask(id: Int!): [Task]
+    updateTask(id: Int!, edits: EditTaskInput!): Task
+    addSubtask(subtask: AddSubtaskInput!): Subtask
+    deleteSubtask(id: Int!): Subtask
+    updateSubtask(id: Int!, edits: EditSubtaskInput!): Subtask
+    reorderSubtasks(taskId: Int!, subtaskId: Int!, newIndex: Int! ): Subtask
+  }
+  input AddTaskInput {
+    title: String!
+    label: String!
+    priority: String!
+    dueDate: String
+  }
+  input EditTaskInput {
+    title: String
+    label: String
+    priority: String
+    dueDate: String
+    status: String
+    progress: Int
+    cursor: Int
+  }
+  input AddSubtaskInput {
+    taskId: Int!
+    title: String!
+  }
+  input EditSubtaskInput {
+    title: String
+    status: String
   }
 `;
